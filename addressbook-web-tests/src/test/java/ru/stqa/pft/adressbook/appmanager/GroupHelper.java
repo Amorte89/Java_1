60f8a66b4038608e698f20e754421b694321376f
Nikitakad <nikita.kadilnikov@inventi.cz>
2020-02-17 17:36:03 +0100

    Добавлены классы-помощники NavigationHelper, GroupHelper, ContactHelper, BaseHelper.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/GroupHelper.java
@@ -0,0 +1,89 @@
+package ru.stqa.pft.adressbook.appmanager;
+
+import com.sun.javafx.binding.ExpressionHelperBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import ru.stqa.pft.adressbook.model.ContactData;
+import ru.stqa.pft.adressbook.model.GroupData;
+
+public class GroupHelper extends HelperBase{
+
+    public GroupHelper(WebDriver wd) {
+        super(wd);
+    }
+
+    public void returnToGroupPage() {
+        click(By.linkText("group page"));
+    }
+
+    public void submitGroupCreation() {
+      click(By.name("submit"));
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        type(By.name("group_name"), groupData.getName());
+        type(By.name("group_header"), groupData.getHeader());
+        type(By.name("group_footer"), groupData.getFooter());
+    }
+
+    public void initGroupCreation() {
+        click(By.name("new"));
+    }
+
+    public void fillContactForm(ContactData contactData) {
+        type(By.name("firstname"), contactData.getFirstname());
+        type(By.name("middlename"), contactData.getMiddlename());
+        type(By.name("lastname"), contactData.getLastname());
+        type(By.name("nickname"), contactData.getNickname());
+        type(By.name("title"), contactData.getTitle());
+        type(By.name("company"), contactData.getCompany());
+        type(By.name("address"), contactData.getAddress1());
+        type(By.name("home"), contactData.getHomephone());
+        type(By.name("mobile"), contactData.getMobilephone());
+        type(By.name("work"), contactData.getWorkphone());
+        type(By.name("fax"), contactData.getFax());
+        type(By.name("email"), contactData.getEmail1());
+        type(By.name("email2"), contactData.getEmail2());
+        type(By.name("email3"), contactData.getEmail3());
+        type(By.name("homepage"), contactData.getHomepage());
+        click(By.name("bday"));
+        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
+        click(By.name("bday"));
+        click(By.name("bmonth"));
+        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
+        click(By.name("bmonth"));
+        click(By.name("byear"));
+        wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
+        click(By.name("aday"));
+        new Select(wd.findElement(By.name("aday"))).selectByVisibleText(contactData.getAday());
+        click(By.name("aday"));
+        click(By.name("amonth"));
+        new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(contactData.getAmouth());
+        type(By.name("ayear"), contactData.getAyear());
+        type(By.name("address2"), contactData.getAddress2());
+        wd.findElement(By.name("phone2")).clear();
+      wd.findElement(By.name("phone2")).sendKeys(contactData.getPhone2());
+        type(By.name("notes"), contactData.getNotes());
+    }
+
+    public void returnToHomePage() {
+        click(By.linkText("home page"));
+    }
+
+    public void submitContactCreation() {
+        click(By.xpath("(//input[@name='submit'])[2]"));
+    }
+
+    public void newContactCreation() {
+        click(By.linkText("add new"));
+    }
+
+    public void deleteSelectedGroups() {
+        click(By.name("delete"));
+    }
+
+    public void selectGroup() {
+        click(By.name("selected[]"));
+    }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
@@ -2,11 +2,12 @@ package ru.stqa.pft.adressbook.tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.adressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.adressbook.appmanager.ApplicationHelper;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationHelper app = new ApplicationHelper();
+
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
