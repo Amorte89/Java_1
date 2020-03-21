3f4f50a35849e6ef59b0159d1c946e4ca0c4bd9d
Nikitakad <nikita.kadilnikov@inventi.cz>
2020-03-21 21:16:04 +0100

    Добавлен конфигурационный файл для запуска тестов.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationHelper.java
@@ -6,10 +6,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationHelper {
 
+    private final Properties properties;
     protected WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -20,9 +25,14 @@ public class ApplicationHelper {
 
     public ApplicationHelper(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+
         if (this.browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (this.browser.equals(BrowserType.CHROME)) {
@@ -32,12 +42,12 @@ public class ApplicationHelper {
         }
 
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook");
+        wd.get(properties.getProperty("web.BaseUrl"));
         this.groupHelper = new GroupHelper(wd, this);
         this.contactHelper = new ContactHelper(wd, this);
         this.navigationHelper = new NavigationHelper(wd, this);
         this.sessionHelper = new SessionHelper(wd, this);
-        this.sessionHelper.login("admin", "secret");
+        this.sessionHelper.login(properties.getProperty("web.AdminLogin"), properties.getProperty("web.AdminPassword"));
     }
 
     public void stop() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
@@ -5,14 +5,16 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.adressbook.appmanager.ApplicationHelper;
 
+import java.io.IOException;
+
 
 public class TestBase {
 
-    protected static final ApplicationHelper app = new ApplicationHelper(BrowserType.CHROME);
+    protected static final ApplicationHelper app = new ApplicationHelper(System.getProperty("browser", BrowserType.CHROME));
 
 
     @BeforeSuite
-    public void setUp() {
+    public void setUp() throws IOException {
         app.init();
     }
 
